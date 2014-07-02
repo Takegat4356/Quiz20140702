d0746d740e84762523cc3029621c490c498aca88
Takegat4356 <ssrankin@umich.edu>
2014-07-02 16:53:02 -0400

    Commit

---
+++ Quiz20140702/ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a console app that compares 2 book objects by their price. 
+//Return 0 if the prices are the same, 1 if the first book is more 
+//expensive than the second and -1 if the first book is cheaper than the second book.
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Price1;
+                        
+            Console.WriteLine("Enter Book 1's Price:");
+            Console.ReadLine();
+
+            //Price will be a descriptor of book
+        }
+    }
+    class Book //Create a new class named Book
+    {
+
+    }
+}
---
+++ Quiz20140702/Quiz20140702/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz20140702
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Integer1;
+            int Integer2;
+
+            Console.WriteLine("Please Enter the first Integer:");
+            Console.ReadLine();
+
+            Console.WriteLine("Please Enter the second Integer:");
+            Console.ReadLine();             
+        }
+    }
+}
