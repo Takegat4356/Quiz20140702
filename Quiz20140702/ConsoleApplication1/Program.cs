51c40732f1b567ff65e51a9ea76c9d558b9d5ee7
Takegat4356 <ssrankin@umich.edu>
2014-07-07 10:57:53 -0400

    Updated my Quiz

--- Quiz20140702/ConsoleApplication1/Program.cs
+++ Quiz20140702/ConsoleApplication1/Program.cs
@@ -10,14 +10,24 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int Integer1;
-            int Integer2;
+            int integer1;
+            int integer2;
+            int min;
 
-            Console.WriteLine("Please Enter the first Integer:");
-            Console.ReadLine();
+            Console.WriteLine("Please Enter the first Integer: " + '\n');
+            integer1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please Enter the second Integer:");
-            Console.ReadLine();             
+            Console.WriteLine('\n' + "Please Enter the second Integer: " + '\n');
+            integer2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine('\n' + "The new integers you entered were: " + integer1 + " and " + integer2);
+            //Console.ReadLine();
+
+            min = Math.Min(integer1, integer2);
+
+            Console.WriteLine('\n' + "The smallest integer is: " + min + '\n');
+
+            Console.ReadLine();       
         }
     }
 }
