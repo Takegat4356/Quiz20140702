7cc924b08e5f91c5105e9bb4b411dd96dfa6c534
Takegat4356 <ssrankin@umich.edu>
2014-07-07 11:08:48 -0400

    Updates to Quiz 7-7-2014 11:08 am

--- Quiz20140702/ConsoleApplication2/Program.cs
+++ Quiz20140702/ConsoleApplication2/Program.cs
@@ -4,26 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Write a console app that compares 2 book objects by their price. 
-//Return 0 if the prices are the same, 1 if the first book is more 
-//expensive than the second and -1 if the first book is cheaper than the second book.
+//Write a console application that compares 2 Book objects by their Price. 
+//Return 0 if the prices are the same, 1 if the first book is more expensive 
+//than the second and -1 if the first book is cheaper than the second book.
 
-namespace ConsoleApplication2
+namespace ConsoleApplication2_Redo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            float Price1;
-                        
-            Console.WriteLine("Enter Book 1's Price:");
-            Console.ReadLine();
+            string book1;
+            string book2;
+            decimal price1;
+            decimal price2;
 
-            //Price will be a descriptor of book
-        }
-    }
-    class Book //Create a new class named Book
-    {
+            //Enter First Book's Information
+            Console.WriteLine("Please Enter your first book: " + '\n');
+            book1 = Console.ReadLine();
 
-    }
+            Console.WriteLine('\n' + "Please Enter your first book's price: " + '\n');
+            price1 = decimal.Parse(Console.ReadLine());
+
+            //Enter Second Book's Information
+            Console.WriteLine('\n' + "Please Enter your second book: " + '\n');
+            book2 = Console.ReadLine();
+
+            Console.WriteLine('\n' + "Please Enter your second book's price: " + '\n');
+            price2 = decimal.Parse(Console.ReadLine());
+
+            //Print out First and Second Book's Information to Confirm Choices
+
+            Console.WriteLine('\n' + "First book's name: " + book1 + '\n' + "First book's price: " + price1);
+            //Console.ReadLine();
+
+            Console.WriteLine('\n' + "Second book's name: " + book2 + '\n' + "Second book's price: " + price2);
+
+            //Return 0 if the prices are the same, 1 if the first book is more expensive 
+            //than the second and -1 if the first book is cheaper than the second book.
+
+            if (price1 == price2)
+            {
+                Console.WriteLine('\n' + "The 2 book prices are the same: Returning 0");
+            }
+            else if (price1 > price2)
+            {
+                Console.WriteLine('\n' + "The first book is more expensive than the second book: Returning 1");
+            }
+            else //if (price1 < price2)
+            {
+                Console.WriteLine('\n' + "The second book is more expensive than the first book: Returning -1");
+            }
+
+            Console.ReadLine();           
+
 }
--- Quiz20140702/ConsoleApplication3/Program.cs
+++ Quiz20140702/ConsoleApplication3/Program.cs
@@ -4,44 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Write a console application that writes to the console the numbers from 1 to 100. But for multiples 
-//of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers 
-//which are multiples of both three and five print "FizzBuzz". 
-//determine if abstract number is abstract multiple of 3
+//Write a console application that writes to the console the numbers from 1 to 100. 
+//But for multiples of three print “Fizz” instead of the number and for the multiples 
+//of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 
-
-namespace ConsoleApplication3
+namespace ConsoleApplication3_Redo
 {
     class Program
     {
         static void Main(string[] args)
         {
-                     /*
 
-        for (int i = 1; i <= 100; i++)
+            for (int i = 1; i < 101; i++)
             {
-        if (i % 5 == 0 && i % 3 == 0)
-        {
-            Console.WriteLine("FizzBuzz");
-        }
-        else if (i % 5 == 0)
-        {
-            Console.WriteLine("Buzz");
-        }
-        else if (i % 3 == 0)
-        {
-            Console.WriteLine("Fizz");
-        } else {
-            Console.WriteLine(i);*/
-
-         for (int i = 1; i < 101; i++)
-            {
-                Console.Write(i + "\t");
-                Console.WriteLine();
+                if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else
+                    Console.WriteLine(i);
             }
 
-            Console.Read();   
-
+            Console.ReadLine();
         }
     }
 }
